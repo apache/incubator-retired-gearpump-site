6dc6ca03c1dacda1c66ec0c27fba3d90764b7498
Sean Zhong <clockfly@gmail.com>
2015-11-17 10:44:49 +0800

    remove _site

--- _site/build_doc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# generate _site documents
-jekyll build
-
-# check html link validality
-htmlproof _site \
-  --disable-external \
-  --url-ignore \#,./../releases/latest/index.html,../../../../releases/latest/index.html
